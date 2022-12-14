090e831e5080de4e4109fb9bb192962ca2d4245b
nac <chotpisit080916c@gmail.com>
2022-12-14 19:53:08 +0700

    finish css part

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,12 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import Script from 'next/script';
+import Layout from '../../components/layout';
 
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
         <Head>
             <title>First Post</title>
             
@@ -21,6 +22,6 @@ export default function FirstPost() {
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
