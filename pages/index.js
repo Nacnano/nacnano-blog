21f3c83036a0aec6cb07ae04125682c14fa1f927
nac <chotpisit080916c@gmail.com>
2022-12-22 00:44:35 +0700

    edit nac

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,10 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Hi, I am Nac. I am still learning Web Development.]</p>
+        <p>Hi, I am Nac. I am still learning Web Development.</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (This is a sample website based on {' '}
+          <a href="https://nextjs.org/learn">Next.js tutorial</a>.)
         </p>
       </section>
 
