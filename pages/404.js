a3d1d42fdc0eb8eec3820b5cc3e0832c4bded68e
nac <chotpisit080916c@gmail.com>
2022-12-22 00:05:00 +0700

    add back to home

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,10 @@
+import styles from '../components/layout.module.css';
+import Link from 'next/link';
 export default function Custom404() {
-    return <h1>404 - Page Not Found go back</h1>;
+    return <>
+    <h1>404 - Page Not Found </h1>
+    <div className={styles.backToHome}>
+        <Link href="/">Back to home</Link>
+    </div>
+    </>;
   }
